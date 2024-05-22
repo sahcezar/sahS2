d2bdd9414731db5f1a28992ace5a24daa6d65d86
sahcezar <00001093273343sp@al.educacao.sp.gov.br>
2024-05-22 18:14:42 -0300

    adicionando arquivos js e css

---
+++ desenhe.js
@@ -0,0 +1,13 @@
+function setup() {
+    createCanvas(600, 600);
+     background("purple");
+  }
+  
+  function draw() {
+   stroke("white");
+    fill("pink");
+    
+    if(mouseIsPressed){
+      rect(mouseX, mouseY, 20, 35);
+    } 
+  }
